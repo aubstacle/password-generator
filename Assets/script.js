c8e08e86a11fdf508215b10690d83dd2742d578c
Aubrey Polk <aubstacle3@gmail.com>
2020-09-03 20:54:55 -0400

    created variables for confirming char types inside generate function

--- Assets/script.js
+++ Assets/script.js
@@ -7,7 +7,24 @@ var numbers = "1234567890";
 var specialCharacters = "?.><,:;}]{[+=_-)(*&^%$#@!~`";
 
 function generatePassword() {
-  var passwordLength = prompt("How long do you want your password to be? Min: 8 characters, Max:128 characters")
+  var passwordLength = prompt("How long do you want your password to be? Min: 8 characters, Max: 128 characters");
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    prompt("Please select a password length between 8-128 characters")
+  }
+
+  var confirmLowerCase = confirm("Do you want lower case letters?");
+
+  var confirmUpperCase = confirm("Do you want UPPER CASE LETTERS?");
+
+  var confirmNumbers = confirm("Do you want numbers?");
+
+  var confirmSpecials = confirm("Do you want special characters?");
+
+  
+
+
+
 }
 
 // Write password to the #password input
